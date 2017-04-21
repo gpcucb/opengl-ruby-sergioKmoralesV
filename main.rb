2f43a76497134b6b01c2b3adaa8a3e02a7627959
sergioKmoralesV <s.mora.94@gmail.com>
2017-04-20 20:25:03 -0400

    Drawing triangle

--- main.rb
+++ main.rb
@@ -7,3 +7,40 @@ include Glu
 include Glut
 
 puts "Hola Mundo"
+
+
+def display
+  #Color de fondo: negro
+  glClearColor(0.0, 0.0, 0.0, 0.0)
+  #Limpiamos la pantalla
+  glClear(GL_COLOR_BUFFER_BIT)
+  #Modo proyección
+  glMatrixMode(GL_PROJECTION)
+  #Cargamos la matriz identidad
+  glLoadIdentity
+  #Proyección ortográfica dentro del cubo señalado
+  glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0)
+
+  #Modo Modelado
+  glMatrixMode(GL_MODELVIEW)
+
+  #Dibujamos un triángulo
+  glBegin(GL_TRIANGLES)
+    glColor3f(1.0,0.0,0.0)
+    glVertex3f(0.0,0.8,0.0)
+    glColor3f(0.0,1.0,0.0)
+    glVertex3f(-0.6,-0.2,0.0)
+    glColor3f(0.0,0.0,1.0)
+    glVertex3f(0.6,-0.2,0.0)
+  glEnd
+  glFlush
+  glutPostRedisplay #para evitar que el ciclo siga corriendo
+end
+
+glutInit
+glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA)
+glutInitWindowSize(500,500)
+glutInitWindowPosition(20,20)
+glutCreateWindow("Hola OpenGL, en Ruby")
+glutDisplayFunc :display
+glutMainLoop
